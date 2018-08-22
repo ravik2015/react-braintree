7aed59af3ed0f82ac3f44651720704b5696f9c3d
Ravi Kumar <ravi.kumar@smartdatainc.net>
2018-08-22 19:27:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import { render } from "react-dom";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Braintree, HostedField } from "react-braintree-fields";
+var dropin = require("braintree-web-drop-in");
+
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    console.log(dropin, "dropindropindropindropin");
+    this.state = { instance: null };
+  }
+
+  componentDidMount() {}
+
+  clickUI = () => {
+    // dropin.create(
+    //   {
+    //     authorization: "sandbox_k3zyhsxn_95csy7fyrgt8cy7c",
+    //     container: "#dropin-container"
+    //   },
+    //   (createErr, instance) => {
+    //     console.log(instance, createErr);
+    //   }
+    // );
+    dropin.create(
+      {
+        authorization: "sandbox_k3zyhsxn_95csy7fyrgt8cy7c",
+        container: "#dropin-container",
+        paypal: {
+          flow: "checkout",
+          amount: "10.00",
+          currency: "USD"
+        },
+        translations: {
+          payingWith: "You are paying with {{paymentSource}}",
+          chooseAnotherWayToPay: "My custom chooseAnotherWayToPay string"
+          // Any other custom translation strings
+        }
+      },
+      (createErr, instance) => {
+        console.log(createErr, instance, "instanceinstance");
+        this.setState({ instance });
+        instance.requestPaymentMethod(function(
+          requestPaymentMethodErr,
+          payload
+        ) {
+          // Submit payload.nonce to your server
+          console.log(requestPaymentMethodErr, payload);
+        });
+      }
+      // (createErr, instance) => {
+      //   instance.requestPaymentMethod(function(
+      //     requestPaymentMethodErr,
+      //     payload
+      //   ) {
+      //     // Submit payload.nonce to your server
+      //     console.log(requestPaymentMethodErr, payload);
+      //   });
+      // }
+    );
+  };
+  pay = () => {
+    let instance = this.state.instance;
+    instance.requestPaymentMethod(function(err, payload) {
+      console.log(err, payload);
+      if (err) {
+        // Handle errors in requesting payment method
+      }
+
+      // Send payload.nonce to your server
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Braintree Payment</h1>
+        <div className="footer">
+          <button onClick={this.clickUI}>Drop IN</button>
+          <button onClick={this.pay}>Pay</button>
+        </div>
+        <div id="dropin-container" />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
+
+export default App;
